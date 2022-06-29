172a05cc7ba1cdcd3771cb55e826fe0ad305fb76
Sehyun Chung <sehyun@berkeley.edu>
2022-06-28 22:57:26 -0400

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,37 @@
-import * as React from "react"
-import {
-  ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import ismobilejs from 'ismobilejs'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from 'views/Home'
 
-export const App = () => (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p={3}>
-        <ColorModeSwitcher justifySelf="flex-end" />
-        <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
-          <Text>
-            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
-          </Text>
-          <Link
-            color="teal.500"
-            href="https://chakra-ui.com"
-            fontSize="2xl"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn Chakra
-          </Link>
-        </VStack>
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+const theme = extendTheme({
+  config: {
+    useSystemColorMode: true,
+    initialColorMode: 'dark',
+  },
+})
+
+export const App = () => {
+  const isMobile = ismobilejs(window.navigator).any
+
+  if (isMobile) {
+    return (
+      <ChakraProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </BrowserRouter>
+      </ChakraProvider>
+    )
+  }
+
+  return (
+    <a
+      href="https://www.browserstack.com/guide/view-mobile-version-of-website-on-chrome"
+      target="_blank"
+      rel="noreferrer"
+    >
+      switch to mobile mode! (and refresh)
+    </a>
+  )
+}
