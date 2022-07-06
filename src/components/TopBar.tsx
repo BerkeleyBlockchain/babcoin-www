f7f9ee02b27330512299956700dcfe53877e9148
Heather Ding <heather.ding@berkeley.edu>
2022-07-05 21:56:13 -0700

    got shape in finally

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -22,9 +22,22 @@ import {
   Icon,
 } from '@chakra-ui/react';
 
-import { createIcon } from '@chakra-ui/icons'
+
 
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import { createIcon } from '@chakra-ui/icons'
+
+// using `path`
+export const CircleIcon = createIcon({
+  displayName: 'circleIcon',
+  viewBox: '0 0 428 774',
+  // path can also be an array of elements, if you have multiple paths, lines, shapes, etc.
+  path: (
+    <path
+      d="M147.5 724.5C60.4724 665.774 13 583 0.363159 499.5V0H472V758.322C407.988 771.319 253.115 795.768 147.5 724.5Z"
+      fill="#FECB33" />
+  ),
+})
 
 type Props = {}
 
@@ -32,30 +45,6 @@ const TopBar = (props: Props) => {
 
   const { isOpen, onOpen, onClose } = useDisclosure();
 
-  
-
-  const HalfCircle = () => (
-    <Icon viewBox='0 0 200 200' color='#FECB33'>
-      <path
-        d="M147.5 724.5C60.4724 665.774 13 583 0.363159 499.5V0H472V758.322C407.988 771.319 253.115 795.768 147.5 724.5Z"
-        fill="#FECB33" />
-    </Icon>
-  )
-
-
-  const NavLink = ({ children }: { children: ReactNode }) => (
-  <Link
-    px={2}
-    py={1}
-    rounded={'md'}
-    _hover={{
-      textDecoration: 'none',
-      bg: useColorModeValue('gray.200', 'gray.700'),
-    }}
-    href={'#'}>
-    {children}
-  </Link>
-);
 
   return (
       <>
@@ -63,23 +52,9 @@ const TopBar = (props: Props) => {
         <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
           <HStack spacing={8} alignItems={'center'}>
             <Box>B@B</Box>
-            
           </HStack>
           <Flex alignItems={'center'}>
-            <Menu>
-              <MenuButton
-                as={Button}
-                colorScheme='black'
-                rounded={'full'}
-                variant={'link'}
-                cursor={'pointer'}
-                minW={0}
-                bg='black'
-                borderColor='black'
-                color='white'
-              >
-            
-                <IconButton
+            <IconButton
                 color='white'
                 bg = 'black'
                 size={'md'}
@@ -88,20 +63,11 @@ const TopBar = (props: Props) => {
                 display={{ md: 'none' }}
                 onClick={isOpen ? onClose : onOpen}
                 />
-              </MenuButton>
-              <MenuList>
-                
-                <MenuItem>Home</MenuItem>
-                <MenuItem>Dashboard</MenuItem>
-                <MenuItem>Admin</MenuItem>
-                <MenuItem>Requirements</MenuItem>
-              </MenuList>
-            </Menu>
+            
           </Flex>
         </Flex>
-
-       
       </Box>
+      <CircleIcon boxSize={200} />
       <Outlet/>
     </>
   );
