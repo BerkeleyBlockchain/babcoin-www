b5bada47f68ccc65ddb7be5e48bb85c8972f9f84
Heather Ding <heather.ding@berkeley.edu>
2022-07-06 21:57:56 -0700

    some type of animation going

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -23,6 +23,7 @@ import { extendTheme } from "@chakra-ui/react"
 
 import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
 import { createIcon } from '@chakra-ui/icons'
+import { motion } from 'framer-motion'
 
 // using `path`
 export const CircleIcon = createIcon({
@@ -43,15 +44,12 @@ export const SmallCircleIcon = createIcon({
     <circle cx="34" cy="34" r="34" fill="#FECB33"/>
   ),
 })
-/** 
-export const BabLogo = createIcon({
-  displayName: 'BabLogo',
-  viewBox: '0 0 68 68',
-  path: (
-    <circle cx="34" cy="34" r="34" fill="#FECB33"/>
-  ),
-})
-*/
+
+
+const variants = {
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: "-100%" },
+}
 
 type Props = {}
 
@@ -61,44 +59,50 @@ const TopBar = (props: Props) => {
 
 
   return (
-      <>
-      <Box bg='black' px="15px" pt = "19px">
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <HStack spacing={8} alignItems={'center'}>
-            {!isOpen ? <Image 
-              src="./logo.png"
-              alt='B@B'
-              zIndex={2}
-              px="15px"
+    <>
+      {!isOpen ?
+        <Box bg='black' px="15px" pt="19px">
+          <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+            <HStack spacing={8} alignItems={'center'}>
+              {!isOpen ? <Image
+                src="./logo.png"
+                alt='B@B'
+                zIndex={2}
+                px="15px"
   
-          /> : <></>}
+              /> : <></>}
           
-          </HStack>
-          <Flex alignItems={'center'}>
-            <IconButton
-              size = {'lg'}
+            </HStack>
+            <Flex alignItems={'center'}>
+              <IconButton
+                size={'lg'}
                 color='white'
-              bg='black'
-              colorScheme = 'blackAlpha'
+                bg='black'
+                colorScheme='blackAlpha'
                 
                 icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
                 aria-label={'Open Menu'}
                 display={{ md: 'none' }}
                 onClick={isOpen ? onClose : onOpen}
-                />
+              />
             
+            </Flex>
           </Flex>
-        </Flex>
-      </Box>
+        </Box> : <></>}
      
-        <Box>
-        {isOpen ? (<CircleIcon
+      <Box>
+      <motion.nav
+      animate={isOpen ? "open" : "closed"}
+      variants={variants}
+    >
+      <CircleIcon
           boxSize={750}
-          position="fixed"
+          position="absolute"
           top={0}
           width= "100%">
           
-        </CircleIcon>) : <></>}
+        </CircleIcon>
+    </motion.nav>
         </Box>
         
     
@@ -141,7 +145,7 @@ const TopBar = (props: Props) => {
         >
           <Link to = "Event" ><ListItem>Home</ListItem></Link>
           <Link to = "Dashboard" ><ListItem>Dashboard</ListItem></Link>
-          <Link to = "TopBarLoggedIn" ><ListItem>Admin</ListItem></Link>
+          <Link to = "Admin" ><ListItem>Admin</ListItem></Link>
           <Link to = "Requirements" ><ListItem>Requirements</ListItem></Link>
         </UnorderedList>: <></>}
       </Box>
