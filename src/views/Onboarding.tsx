20033ca31b94ebf52434fc0427c63d7ca766d02b
Sehyun Chung <sehyun@berkeley.edu>
2022-07-12 21:43:56 -0400

    add onboarding page, edge case should render

---
+++ src/views/Onboarding.tsx
@@ -0,0 +1,13 @@
+import { Flex } from '@chakra-ui/react'
+
+type Props = {}
+
+const Onboarding = (props: Props) => {
+  return (
+    <Flex flexDirection="column" left="16px" position="absolute" right="16px">
+      Onboarding
+    </Flex>
+  )
+}
+
+export default Onboarding
