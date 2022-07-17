292a4fb604017579244df59e018a3705b77232b0
Heather Ding <heather.ding@berkeley.edu>
2022-07-17 13:03:29 -0700

    not integrating chakra icons correctly

--- src/views/Onboarding.tsx
+++ src/views/Onboarding.tsx
@@ -1,4 +1,5 @@
-import { Flex, Box, Text } from '@chakra-ui/react'
+import { ChevronLeftIcon } from '@chakra-ui/icons'
+import { Flex, Box, Text, IconButton } from '@chakra-ui/react'
 import { Input } from '@chakra-ui/react'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 
@@ -8,6 +9,13 @@ const Onboarding = (props: Props) => {
   return (
     <Flex flexDirection="column" left="16px" position="absolute" right="16px">
       <Box height="44px" />
+      <IconButton
+  colorScheme='white'
+  aria-label='Back'
+  size='lg'
+  icon={<ChevronLeftIcon/>}
+/>
+      
       <Text fontSize="50px" fontWeight="bold">
         What's your name?
       </Text>
