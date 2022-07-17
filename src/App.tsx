af904bc0c7c6bfbc09ad6e42598fbae0e940577f
Heather Ding <heather.ding@berkeley.edu>
2022-07-17 12:20:38 -0700

    not tested added

--- src/App.tsx
+++ src/App.tsx
@@ -14,11 +14,47 @@ import { useContractReads } from 'wagmi'
 const contractABI = require("../contract-abi.json");
 const mintNftContract = {
   addressOrName: '0xcbAC32Cc56c8f6c9ac127B304AF8bC5A631CE922',
-  contractInterface:  contractABI,
+  contractInterface: contractABI,
+  watch: true,
+  
 }
 
 export const App = () => {
   const isMobile = ismobilejs(window.navigator).any
+  const { data, isError, isLoading } = useContractReads({
+    contracts: [
+      {
+        //takes an address, and uint256 id as inputs
+        ...mintNftContract,
+        functionName: 'balanceOf',
+        args: [2],
+      },
+      {
+        //takes address[] accounts, uint256[] ids as inputs
+        ...mintNftContract,
+        functionName: 'balanceOfBatch',
+        args: [2],
+      },
+      //Idk what this does + feel like we dont need this
+      {
+        ...mintNftContract,
+        functionName: 'supportsInterface',
+        args: [1],
+      },
+      //Idk what this is either + feel like we dont need this
+      {
+        ...mintNftContract,
+        functionName: 'uri',
+        args: [1],
+      },
+    ],
+    onSuccess(data) {
+      console.log('Success', data)
+    },
+    onError(error) {
+      console.log('Error', error)
+    },
+  })
 
   if (isMobile) {
     return (
