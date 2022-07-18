2db2ee4abfd176046c4769ee589e79a0ac4bd3cb
Sehyun Chung <sehyun@berkeley.edu>
2022-07-18 06:33:35 -0400

    cleanup

--- src/views/Onboarding.tsx
+++ src/views/Onboarding.tsx
@@ -1,26 +1,23 @@
-import { ChevronLeftIcon } from '@chakra-ui/icons'
-import { Flex, Box, Text, IconButton } from '@chakra-ui/react'
-import { Input } from '@chakra-ui/react'
+import { Box, Button, Center, Flex, Input, Text } from '@chakra-ui/react'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 
-type Props = {}
-
-const Onboarding = (props: Props) => {
+const Onboarding = () => {
   return (
     <Flex flexDirection="column" left="16px" position="absolute" right="16px">
       <Box height="44px" />
-      <IconButton
-  colorScheme='white'
-  aria-label='Back'
-  size='lg'
-  icon={<ChevronLeftIcon/>}
-/>
-      
       <Text fontSize="50px" fontWeight="bold">
         What's your name?
       </Text>
-      <Input placeholder='Name' />
-      <ConnectButton label="Connect Wallet" />
+      <Box height="24px" />
+      <Input borderColor="#8D9093" placeholder="Name" />
+      <Box height="24px" />
+      <Center>
+        <ConnectButton label="Connect Wallet" />
+      </Center>
+      <Box height="24px" />
+      <Button backgroundColor="white" borderRadius="12px" color="black">
+        Next
+      </Button>
     </Flex>
   )
 }
