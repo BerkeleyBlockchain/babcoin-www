0fb1a37406f41087409e196b05c986dae32e2831
Heather Ding <heather.ding@berkeley.edu>
2022-07-23 12:47:05 -0700

    can get metadata

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -5,6 +5,33 @@ import ProgressBox from './components/ProgressBox'
 import { useAccount } from 'wagmi';
 import { useState } from 'react';
 
+interface OwnedNfts {
+  //List of user's nfts with data(contract address, nft ID, balance of token, uri but is all nested)
+  ownedNfts: Nfts[]
+  totalCount: number
+  blockHash: string
+}
+interface Nfts {
+  contract: Address
+  id: Id
+  balance: string
+  tokenUri: TokenUris
+}
+interface Address {
+  address: string
+}
+interface Id {
+  tokenId: string
+}
+interface TokenUris {
+  raw: string
+}
+
+interface NftMetadata {
+  description: string
+  image: string
+  name: string
+}
 
 const Dashboard = () => {
   //const address = useAccount().address;
@@ -16,20 +43,44 @@ const Dashboard = () => {
   };
   const baseURL = `https:/polygon-mumbai.g.alchemy.com/nft/v2/${apiKey}/getNFTs`;
   const ownerAddr = `${address}`;
-  const withMetadata = "false";
-
+  const withMetadata = "true";
   //data of all the user's nfts
-  const [userNfts, setUserNfts] = useState([]);
-
-  //the users nfts without the metadata
+  const [userNfts, setUserNfts] = useState<OwnedNfts>();
   const fetchURL = `${baseURL}?owner=${ownerAddr}&withMetadata=${withMetadata}`;
   fetch(fetchURL, requestOptions)
-    .then((response: { json: () => any; }) => response.json())
-    .then((response: any) => JSON.stringify(response, null, 2))
+    .then(response => response.json())
     .then((result: any) => setUserNfts(result))
     .catch((error: any) => console.log('error', error))
+  
+  //returns list of the user's nfts, index into this list to get more data
+  //about each particular nft
+  const ownedNfts = userNfts?.ownedNfts
+  console.log(userNfts?.ownedNfts)
 
-  console.log(userNfts)
+  //number of Different nft's/tokens user has
+  const totalNumDiffTokens = userNfts?.ownedNfts.length
+  console.log(userNfts?.ownedNfts)
+
+  //get the token id number of an owned nft(by index)
+  let idNumber = Number(userNfts?.ownedNfts[1].id.tokenId)
+  console.log(Number(userNfts?.ownedNfts[1].id.tokenId))
+
+  //token metadata URL of a specific token
+  const metadataURL = userNfts?.ownedNfts[1].tokenUri.raw.replace("{id}",`${idNumber}`)
+  console.log(userNfts?.ownedNfts[1].tokenUri.raw.replace("{id}", `${idNumber}`))
+  
+  const[nftMetadata, setNftMetadata] = useState<NftMetadata>()
+  fetch(`${metadataURL}`, requestOptions)
+    .then(response => response.json())
+    .then((result: any) => setNftMetadata(result))
+    .catch((error: any) => console.log('error', error))
+  console.log(nftMetadata?.description)
+  console.log(nftMetadata?.image)
+  console.log(nftMetadata?.name)
+  
+  
+  
+
 
 
 
