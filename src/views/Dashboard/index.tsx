33236f954941dc883281617d1dfde844a617e456
Heather Ding <heather.ding@berkeley.edu>
2022-07-23 02:04:54 -0700

    can fetch user nfts but haven't figured out how to get metadata yet, will probably go through collection and get all metadata for all the possible nft and have logic just match what user has

--- src/views/Dashboard/index.tsx
+++ src/views/Dashboard/index.tsx
@@ -2,7 +2,57 @@ import { Box, Flex, Text } from '@chakra-ui/react'
 import NftGallery from './components/NftGallery'
 import ProgressBox from './components/ProgressBox'
 
+import { useAccount } from 'wagmi';
+import { useState } from 'react';
+
+
 const Dashboard = () => {
+  //const address = useAccount().address;
+  const address = "0xbab0BAe604066BFd4e536Cc1CddfA14D46790E1f"
+  const apiKey = "9_w25dPpnMio1K3JY9FifDnL1U7rlaP2"
+  var requestOptions = {
+    method: 'GET',
+    RequestRedirect: 'follow',
+  };
+  const baseURL = `https:/polygon-mumbai.g.alchemy.com/nft/v2/${apiKey}/getNFTs`;
+  const ownerAddr = `${address}`;
+  const withMetadata = "false";
+
+  //data of all the user's nfts
+  const [userNfts, setUserNfts] = useState([]);
+
+  //the users nfts without the metadata
+  const fetchURL = `${baseURL}?owner=${ownerAddr}&withMetadata=${withMetadata}`;
+  fetch(fetchURL, requestOptions)
+    .then((response: { json: () => any; }) => response.json())
+    .then((response: any) => JSON.stringify(response, null, 2))
+    .then((result: any) => setUserNfts(result))
+    .catch((error: any) => console.log('error', error))
+
+  console.log(userNfts)
+
+
+//data of all the user's nfts
+const [userNftMetadata, setUserNftMetadata] = useState([]);
+
+//Fetch the user's owned nft metadata
+//I think there is a better way to iterate here, like simultaneously though the contract address
+//and corresponding token id
+for (let i = 1; i < 4; i++) {
+  const contractAddr = "0x8a25dcb234b2d3f7d3a8a6bf0c592adcaf20aafb"
+  const tokenId = `${i}`;
+  const tokenType = "erc1155";
+  const baseURLM = `https://polygon-mumbai.alchemyapi.io/nft/v2/${apiKey}/getNFTMetadata`;
+  const fetchMetadataURL = `${baseURLM}?contractAddress=${contractAddr}&tokenId=${tokenId}&tokenType=${tokenType}`;
+  fetch(fetchMetadataURL, requestOptions)
+  .then(response => response.json())
+  .then(response => JSON.stringify(response, null, 2))
+  .then(result => console.log(`${i}:${result}`))
+  .catch(error => console.log('error', error));
+}
+
+
+
   return (
     <Flex
       flexDirection="column"
