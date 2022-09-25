c0c8bf44c439a9694140970dffe5dc418d879baf
Timothy G <timg51237@gmail.com>
2022-09-24 18:09:32 -0700

    fixed nft rendering issue

--- src/views/Dashboard/components/NftGallery.tsx
+++ src/views/Dashboard/components/NftGallery.tsx
@@ -3,6 +3,9 @@ import { useCallback, useEffect, useMemo, useState } from 'react'
 import { Box, Center, HStack, Image, Text } from '@chakra-ui/react'
 
 import { IdToMetadataMap, Metadata, Nfts } from 'types'
+import { useContractRead } from 'wagmi'
+import BabCoinContract from '../../../contracts/BabCoinContract.json'
+import useDatabase from 'contexts/database/useDatabase'
 
 const key = '9_w25dPpnMio1K3JY9FifDnL1U7rlaP2'
 const BASE_URL = `https:/polygon-mumbai.g.alchemy.com/nft/v2/${key}/getNFTs`
@@ -18,15 +21,29 @@ const NftGallery: React.FC<Props> = ({ account }) => {
   const [metadata, setMetadata] = useState<IdToMetadataMap>({})
   console.log('🚀 ~ metadata', metadata)
   const [userNfts, setUserNfts] = useState<Nfts[]>([])
+  const { attendedEvents } = useDatabase()
 
   const handleFetchNfts = useCallback(async () => {
+    console.log('fetching')
     const res = await fetch(`${BASE_URL}?owner=${account}&withMetadata=true`, {
       method: 'GET',
     })
       .then((response) => response.json())
       .catch((error) => console.log(error))
+    console.log(res)
     setUserNfts(res.ownedNfts as Nfts[])
   }, [account])
+
+  // const data = useContractRead({
+  //   addressOrName: '0x5147E15E6a37D346E0142492c63D6f660b5C9bd3',
+  //   contractInterface: BabCoinContract.abi,
+  //   functionName: 'uri',
+  //   args: 1, //[account, 0],
+  //   onSettled(data, error) {
+  //     console.log('Settled', { data, error })
+  //   },
+  // })
+  // console.log(data)
 
   const handleFetchMetadata = useCallback(async () => {
     if (!userNfts) return
@@ -53,16 +70,20 @@ const NftGallery: React.FC<Props> = ({ account }) => {
     await Promise.all(promises)
   }, [userNfts])
 
-  useEffect(() => {
-    handleFetchMetadata()
-  }, [handleFetchMetadata])
+  // useEffect(() => {
+  //   handleFetchMetadata()
+  // }, [handleFetchMetadata])
 
-  useEffect(() => {
-    handleFetchNfts()
-  }, [handleFetchNfts])
+  // useEffect(() => {
+  //   handleFetchNfts()
+  // }, [handleFetchNfts])
+
+  console.log(attendedEvents)
+
+  console.log()
 
   const Gallery = useMemo(() => {
-    if (!Object.values(metadata).length) {
+    if (!attendedEvents.length) {
       return (
         <Center paddingTop="12">
           <Text>View your NFTs here!</Text>
@@ -71,16 +92,25 @@ const NftGallery: React.FC<Props> = ({ account }) => {
     }
     return (
       <HStack spacing="25px">
-        {Object.values(metadata).map((metadata) => (
+        {/* {Object.values(metadata).map((metadata) => (
           <Image
             alt={metadata.name}
             key={metadata.image}
             src={metadata.image || fallbackUrl}
           />
+        ))} */}
+
+        {attendedEvents.map((event) => (
+          <Image
+            alt={event.name}
+            key={event.nftArtUrl}
+            src={event.nftArtUrl || fallbackUrl}
+            style={{ width: '80vw', margin: '10vw 15vw 10vw 5vw' }}
+          />
         ))}
       </HStack>
     )
-  }, [metadata])
+  }, [attendedEvents])
 
   return (
     <Box overflowX="auto" whiteSpace="nowrap">
